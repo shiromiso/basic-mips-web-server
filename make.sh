4158bc8e72cecd627a2a403fa494cbe0ea74405a
Michael <iline.mic@gmail.com>
2023-04-22 03:42:59 +0300

    Make process exit on failure

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
 #/bin/bash
-mipsel-linux-gnu-as main.s -o main.o
-mipsel-linux-gnu-gcc main.o -o main -nostdlib -static
+mipsel-linux-gnu-as main.s -o main.o && mipsel-linux-gnu-gcc main.o -o main -nostdlib -static
