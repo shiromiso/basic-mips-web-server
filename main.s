c53e8b784dda94d5e6568fd2f6c115eeb80a3f0d
Michael <iline.mic@gmail.com>
2023-04-22 04:08:04 +0300

    Add functions

--- main.s
+++ main.s
@@ -8,12 +8,22 @@ msg:
 .global __start
 
 __start:
+main:
+	jal print
+	jal exit
+
+exit:
+	li $v0, 4001
+	li $a0, 13
+	syscall
+
+	jr $ra
+
+print:
 	li $v0, 4004
 	li $a0, 1
 	la $a1, msg
 	li $a2, 13
 	syscall
 
-	li $v0, 4001
-	li $a0, 13
-	syscall
+	jr $ra
